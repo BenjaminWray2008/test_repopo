55238f000b3249d8d86a628a9191e3f3c816bd08
BenjaminWray2008 <benjamin.wray2008@gmail.com>
2026-02-17 13:20:06 +1300

    broke everything

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, ForeignKey, select
 from sqlalchemy.orm import DeclarativeBase, relationship, mapped_column, Mapped
@@ -35,37 +35,66 @@ class Order(db.Model):
     pizza = relationship("Pizza", back_populates="orders")
 
 
+@app.route('/add', methods=['POST'])
+def add():
+    cn = False
+    names = request.form.get('name')
+    pizza = request.form.get('pizza')
+    pizaas = Pizza.query.all()
+    for pizzas in pizaas:
+        print(pizzas, pizza)
+        if pizza == pizzas.name:
+            nid = pizzas.id
+            cn = True
+    
+        
+    
+    if cn:
+        user = db.session.scalar(select(User).where(User.name == names))
+        print(user)
+        if not user:
+            new_user = User(name=names)
+            db.session.add(new_user)
+            db.session.commit()
+            user = db.session.scalar(select(User).where(User.name == names))
+        
+        print(user.id, nid)
+        nO = Order(user_id=user.id, pizza_id=nid)
+        db.session.add(nO)
+        db.session.commit()
+        db.session.close()
+    return redirect(url_for('index'))
 
-
-@app.route('/')
+@app.route('/index')
 def index():
+    print('hi')
     info = []
-
+    userss = []
+    pzza = []
     pizzas = Pizza.query.all()
     for pizza in pizzas:
-        info.append((pizza.name, pizza.id, pizza.topping))
+        pzza.append((pizza.id, pizza.name, pizza.topping))
+   
     users = User.query.all()
     for user in users:
+        print(user)
         info.append((user.name, user.id))
-    orders = Order.query.all()
-    for order in orders:
-        info.append((order.pizza_id, order.id, order.user_id))
+        userss.append(user.name)
+    
 
 
-    results = User.query.join(User.orders).join(Order.pizza).add_entity(Pizza).all()
+    results = User.query.join(User.orders).join(Order.pizza).add_entity(Pizza).add_entity(Order).all()
     oderings = []
     for i in results:
-        oderings.append((i[0].name, i[1].topping))
+        oderings.append((i[0].name, i[1].topping, i[2].id))
+  
+   
+    
 
     
 
 
-    return render_template('index.html', info=info, results=results, oderings=oderings)
-
-
-
-
-
+    return render_template('index.html', info=info, results=results, oderings=oderings, userss=userss, pzza=pzza)
 
 if __name__ == '__main__':
     app.run(debug=True)
